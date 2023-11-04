bb5997a7224e42afb1eea66cf9e77647009f14ca
dieynaba2002 <dieynabacoly51@gmail.com>
2023-11-04 22:24:53 +0000

    my first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FormsModule } from '@angular/forms';
+import { ListeContactComponent } from './liste-contact/liste-contact.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ListeContactComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/liste-contact/liste-contact.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liste-contact',
+  templateUrl: './liste-contact.component.html',
+  styleUrls: ['./liste-contact.component.css']
+})
+export class ListeContactComponent {
+
+  
+}
